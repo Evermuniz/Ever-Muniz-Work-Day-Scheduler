baa8ab52a30afa7585f7aa7c6f2e7234a4a30305
evermuniz <evermuniz@outlook.com>
2023-03-25 16:12:08 -0600

    added comments to js script

--- script.js
+++ script.js
@@ -4,51 +4,33 @@
 
 
 $(function () {
-  var currentDate = dayjs().format('MMM D, YYYY');
-  $('#currentDay').text(currentDate);
+  var currentDate = dayjs().format('MMM D, YYYY'); //declaring a variable with today's date using dayjs
+  $('#currentDay').text(currentDate); //using jquery to select the p element in HTML with #currentDay id and setting text content to display today's date
 
-
-  $('.saveBtn').on('click', function (){
-    var id = $(this).parent().attr("id");
-    var contents = $(this).siblings("textarea").val();
-    localStorage.setItem(id, JSON.stringify(contents));
-  })
-
-  var currentTime = dayjs().format("HH");
-  console.log(currentTime);
-
-  $('.time-block').each(function() {
-  var timeBlock = $(this).attr("id");
-  console.log(timeBlock);
-  if (currentTime > timeBlock) {$(this).addClass("past");}
-  if (currentTime === timeBlock) {$(this).addClass("present");}
-  if (currentTime < timeBlock) {$(this).addClass("future");}
-
-  var description = $(this).children("textarea");
-  var storedData = JSON.parse(localStorage.getItem(timeBlock));
-  description.text(storedData);
+  $('.saveBtn').on('click', function (){ //using jquery to select all save buttons in HTML and trigger this function when they are clicked
+    var id = $(this).parent().attr("id"); //this variable value is defined  through several steps. First, by which save button was clicked which is why we need "this". 
+    //then we check the button parent, which is the div. Then we check for the value of the id assigned to the same parent. The value will be the respective time slot in HTML
+    var contents = $(this).siblings("textarea").val(); //this variable is checking for the user input in the webpage. Once they have clicked save, then we will 
+    //check the button sibling to make sure we reference the right time slot. The text is then the value of the contents variable
+    localStorage.setItem(id, JSON.stringify(contents)); //We then want to save the input in local storage. By using the id value as the key and the 
+    //contents input as the values then we can easily save and display the input in the respective time slot even after refreshing the page
   });
 
- 
+  var currentTime = dayjs().format("HH"); //creating a new variable with today's date as the value by using day.js library
+  console.log(currentTime);// logging this was very helpful to determine if everything was working as expected
+
+  $('.time-block').each(function() { //using jquery to select all divs with a time-block class. Using the each to repeat this function for every div with this class
+  var timeBlock = $(this).attr("id"); //similar to the id variable used above, we're checking for the id value which gives us the repective time slot.
+  //the only difference is that we are checking this directly in the div and don't need to reference  a relative and only need to use "this"
+  console.log(timeBlock);// also very helpful to check everything is working as expected
+  if (currentTime > timeBlock) {$(this).addClass("past");} //if the current time is greater than the time slot, then apply the "past" class
+  if (currentTime === timeBlock) {$(this).addClass("present");} //if the current time is greater than the time slot, then apply the "present" class
+  if (currentTime < timeBlock) {$(this).addClass("future");} //if the current time is greater than the time slot, then apply the "future" class
+
+  var description = $(this).children("textarea");//Using "this" to select the child element with the "text area  class which is where text is entered and displayed on the page"
+  var storedData = JSON.parse(localStorage.getItem(timeBlock));//Using JSON to parse and retrieve what was saved in local storage earlier
+  //we're using the timeblock variable so we can match the id value from "this" to the same key value in local storage since we used the same id to set it in the first place
+  description.text(storedData); //Finally, we display the value of the respective key in the respective time slot
+  });
+
 });
-
-
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
